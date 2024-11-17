3595d01588ee64a7c6a2bb01a7040e058b310b5a
Michael Mussato <michimussato@gmail.com>
2024-11-17 12:47:03 +1100

    updated packaging

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from distutils.core import setup
-setup(
-  name = 'suncalcPy',
-  packages = ['suncalcPy'], # this must be the same as the name above
-  version = '0.1',
-  description = 'A python library for calculating sun position, sunlight phases, moon position and lunar phases for a given location and time.',
-  author = 'Abe Miessler',
-  author_email = 'abemiesler@gmail.com',
-  url = 'https://github.com/broham/suncalcpy', # use the URL to the github repo
-  download_url = 'https://github.com/broham/suncalcpy/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['sun', 'moon', 'phases', 'position', 'time', 'location'], # arbitrary keywords
-  classifiers = [],
-)
+from setuptools import setup
+
+if __name__ == "__main__":
+    try:
+        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+    except:  # noqa
+        print(
+            "\n\nAn error occurred while building the project, "
+            "please ensure you have the most updated version of setuptools, "
+            "setuptools_scm and wheel with:\n"
+            "   pip install -U setuptools setuptools_scm wheel\n\n"
+        )
+        raise
